d7f17fc910e64c54007c36edfec38b42c7060cb8
Donald <448641125@qq.com>
2021-06-13 23:10:26 +0800

    take ribbon note

--- SpringCloud/greeting-client/src/main/java/com/donald/Application.java
+++ SpringCloud/greeting-client/src/main/java/com/donald/Application.java
@@ -1,13 +1,20 @@
 package com.donald;
 
+import com.netflix.client.http.HttpRequest;
+import com.netflix.client.http.HttpResponse;
+import com.netflix.niws.client.http.RestClient;
+import com.netflix.ribbon.ClientOptions;
+import com.netflix.ribbon.Ribbon;
+import com.netflix.ribbon.http.HttpResourceGroup;
+
 /**
  * @author donald
- * @date 2021/06/12
+ * @date 2021/06/13
  */
 public class Application {
 
     public static void main(String[] args) throws Exception {
-        // 基于ribbon的api来开发一个可以负载均衡调用一个服务的代码
+        /*// 基于ribbon的api来开发一个可以负载均衡调用一个服务的代码
         // 首先使用代码的方式对ribbon进行一下配置，配置一下ribbon要调用的那个服务的server list
         ConfigurationManager.getConfigInstance().setProperty(
                 "greeting-service.ribbon.listOfServers", "localhost:8080,localhost:8088");
@@ -25,6 +32,21 @@ public class Application {
             HttpResponse response = restClient.executeWithLoadBalancer(request);
             String result = response.getEntity(String.class);
             System.out.println(result);
-        }
+        }*/
+
+        /*HttpResourceGroup httpResourceGroup = Ribbon.createHttpResourceGroup("movieServiceClient",
+                ClientOptions.create()
+                        .withMaxAutoRetriesNextServer(3)
+                        .withConfigurationBasedServerList("localhost:8080,localhost:8088"));
+        HttpRequestTemplate<ByteBuf> recommendationsByUserIdTemplate = httpResourceGroup.newTemplateBuilder("recommendationsByUserId", ByteBuf.class)
+                .withMethod("GET")
+                .withUriTemplate("/users/{userId}/recommendations")
+                .withFallbackProvider(new RecommendationServiceFallbackHandler())
+                .withResponseValidator(new RecommendationServiceResponseValidator())
+                .build();
+        Observable<ByteBuf> result = recommendationsByUserIdTemplate.requestBuilder()
+                .withRequestProperty("userId", "user1")
+                .build()
+                .observe();*/
     }
 }
